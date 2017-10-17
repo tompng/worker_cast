356c731c762616415800556f1c926bcbabf3740d
tompng <tomoyapenguin@gmail.com>
2017-10-18 02:08:45 +0900

    first try

--- lib/worker_cast.rb
+++ lib/worker_cast.rb
@@ -1,5 +1,158 @@
 require "worker_cast/version"
 
+module WorkerCast::Connection
+  def initialize name, ip, port
+    @name = name
+    @ip = ip
+    @port = port
+    @send_queue = Queue.new
+    @serial_key = 0
+    @mutex = Mutex.new
+    @waitings = {}
+    Thread.new do
+      loop do
+        start rescue nil
+        sleep 1
+      end
+    end
+    Thrad.new do
+      send_loop
+    end
+  end
+
+  def send_loop
+    loop do
+      data = @send_queue.deq
+      begin
+        @socket.puts data.to_json
+      rescue StandardError
+        key = data[1]
+        @mutex.synchronize { @waitings.delete(key) << nil } if key
+      end
+    end
+  end
+
+  def send(message, response_queue = nil)
+    unless @socket
+      response_queue&.<< nil
+      return nil
+    end
+    if response_queue
+      @mutex.synchronize do
+        key = @serial_key += 1
+        @waitings[key] = response_queue
+        @send_queue << [message, key]
+      end
+    else
+      @send_queue << [message]
+    end
+    true
+  end
+
+  def start
+    @socket = TCPSocket.new(@ip, @port)
+    while (data = @socket.gets)
+      message, key = JSON.parse(data)
+      @mutex.synchronize do
+        @waitings.delete(key) << message rescue nil
+      end
+    end
+  ensure
+    @socket.close
+    @socket = nil
+    @mutex.synchronize do
+      @waitings.each_value { |queue| queue << nil rescue nil }
+      @waitings = {}
+    end
+  end
+end
+
 module WorkerCast
-  # Your code goes here...
+  def self.start(servers, self_name, &block)
+    self_ip, self_port = servers[self_name].split ':'
+    @servers = {}
+    @name = self_name
+    @block = block
+    @ondata_queue = Queue.new
+    servers.each do |name, ip_port|
+      ip, port = ip_port.split ':'
+      ip = 'localhost' if ip == self_ip
+      @servers[name] = Connection.new name, ip, port if name != self_name
+    end
+    Thread.new { accept_start self_port }
+    Thread.new { consume }
+  end
+
+  def broadcast message, response: true
+    if response
+      name_queues = @servers.map do |name, server|
+        queue = Queue.new
+        server.send message, queue
+        queue
+      end
+      name_queues.map { |name, queue| [name, queue.deq] }.to_h
+    else
+      @servers.map do |name, server|
+        [name, server.send(message)]
+      end.to_h
+    end
+  end
+
+  def send name, message, response: true
+    if response
+      queue = Queue.new
+      @servers[name].send message, queue
+      queue.deq
+    else
+      @servers[name].send message
+    end
+  end
+
+  def consume
+    loop do
+      message, key, response_queue = @ondata_queue.deq
+      respond = lambda do |response|
+        response_queue << [response, key] rescue nil if key
+        key = nil
+      end
+      response = @block.call message, respond rescue nil
+      respond.call response unless response == :async
+    end
+  end
+
+  def self.accept_start port
+    server = TCPServer.new port
+    loop do
+      onconnect server.accept
+    end
+  end
+
+  def ondata message_key_queue
+    @ondata_queue << message_key_queue
+  end
+
+  def self.onconnect socket
+    response_queue = Queue.new
+    Thread.new do
+      begin
+        loop do
+          message, key = JSON.parse socket.gets
+          ondata [message, key, response_queue]
+        end
+      ensure
+        response_queue.close
+        socket.close
+      end
+    end
+    Thead.new do
+      begin
+        while (data = response_queue.deq)
+          socket.puts data.to_json
+        end
+      ensure
+        response_queue.close
+        socket.close
+      end
+    end
+  end
 end
